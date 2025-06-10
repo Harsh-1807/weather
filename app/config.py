0493c44e1dde59e502b967b1e74f7db3d62f5a15
Harsh-1807 <nkharshbachhav@gmail.com>
2025-06-10 22:25:45 +0530

    Initial commit

--- app/config.py
+++ app/config.py
@@ -117,6 +117,7 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
+        extra = "ignore"  # Ignore extra fields in environment variables
 
 @lru_cache()
 def get_settings():
